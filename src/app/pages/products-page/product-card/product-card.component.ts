4530ad47ea725ebc3fd91521701f75dbf6826b1e
meiry11 <meirycisse11@gmail.com>
2022-06-24 01:06:48 +0200

    fix:premier commit composant

--- src/app/pages/products-page/product-card/product-card.component.ts
+++ src/app/pages/products-page/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
-
+@Input()
+  produitCard:any = "";
   constructor() { }
 
   ngOnInit(): void {
